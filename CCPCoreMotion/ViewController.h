f9a9b37671f65590faaac99ae8f173cc9186f9ad
brian-chu <chuchengpeng@liqunfeidemac-mini.local>
2016-03-04 15:39:34 +0800

    Initial Commit

---
+++ CCPCoreMotion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCPCoreMotion
+//
+//  Created by liqunfei on 16/3/4.
+//  Copyright © 2016年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
